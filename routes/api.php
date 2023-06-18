98e40dc4ac25c8d6636477e0aeb52bda4c170ec3
Hafif I <hafifimammuddyn@gmail.com>
2023-06-18 08:14:11 +0700

    ✨ Added routes for CRUD Jenis Barang, Barang and Transaksi

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('jenis-barang')->group(function () {
+    Route::get('/', 'App\Http\Controllers\JenisBarangController@get');
+    Route::get('/{id}', 'App\Http\Controllers\JenisBarangController@show');
+    Route::post('/', 'App\Http\Controllers\JenisBarangController@store');
+    Route::delete('/{id}', 'App\Http\Controllers\JenisBarangController@destroy');
+    Route::put('/{id}', 'App\Http\Controllers\JenisBarangController@update');
+});
+
+Route::prefix('barang')->group(function () {
+    Route::get('/', 'App\Http\Controllers\BarangController@get');
+    Route::get('/{id}', 'App\Http\Controllers\BarangController@show');
+    Route::post('/', 'App\Http\Controllers\BarangController@store');
+    Route::delete('/{id}', 'App\Http\Controllers\BarangController@destroy');
+    Route::put('/{id}', 'App\Http\Controllers\BarangController@update');
+});
+
+Route::prefix('transaksi')->group(function () {
+    Route::get('/', 'App\Http\Controllers\TransaksiController@get');
+    Route::get('/{id}', 'App\Http\Controllers\TransaksiController@show');
+    Route::post('/', 'App\Http\Controllers\TransaksiController@store');
+    Route::delete('/{id}', 'App\Http\Controllers\TransaksiController@destroy');
+    Route::put('/{id}', 'App\Http\Controllers\TransaksiController@update');
+});
