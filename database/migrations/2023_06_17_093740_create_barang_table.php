f4d983da332af8b2464da97a2a7d9e4b6863cd41
Hafif I <hafifimammuddyn@gmail.com>
2023-06-18 08:08:06 +0700

    ✨ Added migration for create Barang table

---
+++ database/migrations/2023_06_17_093740_create_barang_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->foreignId('id_jenis_barang')->references('id')->on('jenis_barang')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
